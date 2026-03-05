474492f5f986a680b5878b0676164aaedd431055
MoraDiego <diego.fmg@live.com>
2026-03-05 11:39:44 -0500

    feat: validaciones creditos planes

--- src/app/components/plan-estudio-base/plan-estudio-base.component.ts
+++ src/app/components/plan-estudio-base/plan-estudio-base.component.ts
@@ -157,7 +157,7 @@ export abstract class PlanEstudioBaseComponent {
     protected utilidadesService: UtilidadesService
   ) {
     this.formGroupPlanEstudio = new FormGroup({});
-   }
+  }
 
   setRoles() {
     this.autenticationService.getRole().then((rol: any) => {
@@ -1137,19 +1137,42 @@ export abstract class PlanEstudioBaseComponent {
       error: null,
       showPopUp: false,
       messagePopUp: ""
+    };
+    const validPrereq = await this.validarPrerequisitosAgregar(id);
+    if (!validPrereq) {
+      return {
+        ...result,
+        valid: false,
+        error: this.translate.instant(
+          'plan_estudios.error_validacion_prerrequisitos_espacios'
+        )
+      };
     }
-    return new Promise<object>((resolve, reject) => {
-      this.validarPrerequisitosAgregar(id).then((valid) => {
-        if (valid) {
-          resolve(result);
-        } else {
-          result["valid"] = false;
-          result["error"] = this.translate.instant(
-            'plan_estudios.error_validacion_prerrequisitos_espacios');
-          reject(result);
-        }
-      });
-    });
+    const validCreditos = await this.validarTotalCreditosEspacios(id);
+    if (!validCreditos) {
+      return {
+        ...result,
+        valid: false,
+        error: this.translate.instant(
+          'plan_estudios.error_validacion_creditos_espacios'
+        )
+      };
+    }
+    return result;
+  }
+
+  async validarTotalCreditosEspacios(id: any): Promise<boolean> {
+    let currentSpace = this.ListEspacios.find(espacio => espacio._id == id);
+    let creditos = currentSpace["creditos"];
+    const creditosPlan = Number(
+      this.formGroupPlanEstudio.get('totalCreditosPrograma')?.value
+    );
+    let totalCreditosSemestre = this.dataSemestreTotalTotal.data[0].creditos;
+    if (creditosPlan && creditos && (totalCreditosSemestre + creditos) > creditosPlan) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   async validarPrerequisitosAgregar(id: any): Promise<boolean> {
@@ -1328,13 +1351,53 @@ export abstract class PlanEstudioBaseComponent {
       this.dataOrganizedStudyPlans.data = this.dataOrganizedStudyPlans.data
     }
   }
+  async validarTotalCreditosPlan(element: any): Promise<boolean> {
+    let creditosAgregados=element.TotalCreditos
+    let creditosProgramasAgregados=this.dataOrganizedStudyPlans.data
+                              .reduce((total: number, plan: any) => {
+                                return total + Number(plan.TotalCreditos);
+                              }, 0)
+    let totalCreditosPlan = Number(this.formGroupPlanEstudio.get('totalCreditosPrograma')?.value);
+    if ((creditosAgregados + creditosProgramasAgregados) > totalCreditosPlan) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  async runValidations2PlansAdding(element: any): Promise<object> {
+    let result = {
+      valid: true,
+      error: null,
+      showPopUp: false,
+      messagePopUp: ""
+    };
+    const validCreditos = await this.validarTotalCreditosPlan(element);
+    if (!validCreditos) {
+      return {
+        ...result,
+        valid: false,
+        error: this.translate.instant(
+          'plan_estudios.error_validacion_creditos_espacios'
+        )
+      };
+    }
+    return result;
+  }
 
   addPlan(element: any) {
     let newPlan = element;
-    newPlan["orden"] = this.dataOrganizedStudyPlans.data.length + 1;
-    this.dataOrganizedStudyPlans.data.push(newPlan);
-    this.dataOrganizedStudyPlans.data = this.dataOrganizedStudyPlans.data
-    this.dataSimpleStudyPlans.data = this.dataSimpleStudyPlans.data.filter((item: any) => item.Id !== element.Id);
+    this.runValidations2PlansAdding(element).then((result: any) => {
+      if (result["valid"]) {
+      newPlan["orden"] = this.dataOrganizedStudyPlans.data.length + 1;
+      this.dataOrganizedStudyPlans.data.push(newPlan);
+      this.dataOrganizedStudyPlans.data = this.dataOrganizedStudyPlans.data
+      this.dataSimpleStudyPlans.data = this.dataSimpleStudyPlans.data.filter((item: any) => item.Id !== element.Id);
+      } else {
+        this.popUpManager.showErrorAlert(result["error"]);
+      }
+    }).catch((result) => {
+      this.popUpManager.showErrorAlert(result["error"]);
+    });
     //this.dataSimpleStudyPlans.data = this.dataSimpleStudyPlans.data
   }
 
